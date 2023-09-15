01848fdcf9e6e8b793f5aa96d5c1ab093a867371
Pawel Maslak <maslak.pawel89@gmail.com>
2023-09-15 21:25:25 +0200

    Initial project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './core/pages/welcome/welcome.component';
+import { ContactComponent } from './core/pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
